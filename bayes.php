11dc49d1a08cff5011d48c371409a381a6778ead
Excha44 <pso2worldjp@gmail.com>
2017-04-10 16:20:39 +0700

    tata

--- bayes.php
+++ bayes.php
@@ -2,7 +2,7 @@
 include_once "helper/database.php";
 session_start();
 
-//GITHUB
+//tata ancok
 if(@$_POST['pilih_gejala']){
 	$pilih_gejala = @$_POST['pilih_gejala'];
 	$jumlah = count($pilih_gejala);
